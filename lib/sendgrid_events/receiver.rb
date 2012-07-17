7050cf746de8dbce7380daf74ae87f04ee47e481
Jon Phenow <j.phenow@gmail.com>
2012-07-17 14:49:01 -0500

    forgot to include the original param key

--- lib/sendgrid_events/receiver.rb
+++ lib/sendgrid_events/receiver.rb
@@ -2,7 +2,7 @@ module SendgridEvents
   class Receiver
     def self.receive(params)
       if Configure.receive?
-        params.each do |event|
+        params['_json'].each do |event|
           Handlers::Base.choose_and_handle(event) if event.has_key? "sendgrid_events_id"
         end
       end
