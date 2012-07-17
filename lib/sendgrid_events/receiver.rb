0d12bb297cb199e5b19abafc6fec9047b55ca63a
Jon Phenow <j.phenow@gmail.com>
2012-07-17 14:33:54 -0500

    get correct hash obj

--- lib/sendgrid_events/receiver.rb
+++ lib/sendgrid_events/receiver.rb
@@ -3,7 +3,7 @@ module SendgridEvents
     def self.receive(params)
       if Configure.receive?
         params.each do |event|
-          Handlers::Base.choose_and_handle(event) if event.has_key? :sendgrid_events_id
+          Handlers::Base.choose_and_handle(event) if event.has_key? "sendgrid_events_id"
         end
       end
     end
