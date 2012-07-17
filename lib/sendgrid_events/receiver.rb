1274b0647be84f01771ed958b6440e077dfdc5bc
Jon Phenow <j.phenow@gmail.com>
2012-07-17 17:26:11 -0500

    botched the register_handlers

--- lib/sendgrid_events/receiver.rb
+++ lib/sendgrid_events/receiver.rb
@@ -1,10 +1,12 @@
 module SendgridEvents
   class Receiver
     def self.receive(params)
-      if Configure.receive?
+      if Configure.receive? and params['_json'] and params['_json'].is_a? Array
         params['_json'].each do |event|
-          event = event.with_indifferent_access
-          Handlers::Base.choose_and_handle(event) if event.has_key? "sendgrid_events_id"
+          if event.is_a? Hash and event.has_key? "sendgrid_events_id"
+            event = event.with_indifferent_access
+            Handlers::Base.choose_and_handle(event)
+          end
         end
       end
     end
