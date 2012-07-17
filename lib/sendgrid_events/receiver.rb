ef2570d07be17281ba4337d7a7f3588488e7a940
Jon Phenow <j.phenow@gmail.com>
2012-07-17 15:18:18 -0500

    fix test

--- lib/sendgrid_events/receiver.rb
+++ lib/sendgrid_events/receiver.rb
@@ -3,6 +3,7 @@ module SendgridEvents
     def self.receive(params)
       if Configure.receive?
         params['_json'].each do |event|
+          event = event.with_indifferent_access
           Handlers::Base.choose_and_handle(event) if event.has_key? "sendgrid_events_id"
         end
       end
